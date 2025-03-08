7cf19f12e9cde47707e847ec1fa45dc7c4f13f83
Bekay_2011 <bekirsarikaya64@gmail.com>
2025-03-08 14:30:20 +0100

    08.03.2025

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,25 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.print("Hello Java!");
         System.out.println("Hello World!");
+        int a = 5;
+        int b = 15;
+        int sum = a + b;
+
+        System.out.println(sum);
+        System.out.print("Hello World");
+
+        Calc(a, b);
+
+
+    }
+
+    static void Calc(int a, int b){
+
+        System.out.printf("Your numbers to calculate are a = %d and b = %d", a, b);
+        System.out.println();
+        System.out.println("The result of the calculation is = "+ (a+b));
+
     }
 }
